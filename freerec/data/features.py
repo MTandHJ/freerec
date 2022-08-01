7d89397eb994b4940e919300493efce691a5a14d
MTandHJ <congxueric@gmail.com>
2022-08-01 20:53:25 +0800

    modify features

--- freerec/data/features.py
+++ freerec/data/features.py
@@ -1,17 +1,15 @@
 
 
 
-from typing import Callable, Iterable, Tuple, Union, Any
+from typing import Callable, Iterable, Tuple, Union, Any, Dict, List
 
 import torch
 from sklearn.preprocessing import LabelEncoder
 
 from .utils import safe_cast
-from ..dict2obj import Config
 
 
-
-class Field(Config):
+class Field(torch.nn.Module):
 
     def __init__(self, name: str, na_value: Union[str, int, float], dtype: Callable):
         """
@@ -19,8 +17,38 @@ class Field(Config):
         na_value: fill na with na_value
         dtype: str, int ... for safe_cast
         """
-        super().__init__(name=name, na_value=na_value, dtype=dtype)
+        super().__init__()
 
+        self.__name = name
+        self.__na_value = na_value
+        self.__dtype = dtype
+
+    @classmethod
+    def filter(cls, fields: Iterable, strict: bool = False):
+        """
+        strict == True:
+            return fields whose class is exactly 'cls', i.e., calling 'type(field) == cls'
+        strict == False:
+            return fields inherited from 'cls', i.e., calling 'isinstance(field, cls)'
+        """
+        def _check_instance(instance):
+            return isinstance(instance, cls)
+        def _check_type(instance):
+            return type(instance) == cls
+        func = _check_type if strict else _check_instance
+        return filter(func, fields)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def na_value(self):
+        return self.__na_value
+    
+    @property
+    def dtype(self):
+        return self.__dtype
 
     def __call__(self, val: Any) -> Any:
         return safe_cast(val, self.dtype, self.na_value)
@@ -37,9 +65,16 @@ class SparseField(Field):
         self.transformer = LabelEncoder()
         self.transformer.fit(enum)
 
-
     def transform(self, x: Iterable) -> torch.Tensor:
         return torch.from_numpy(self.transformer.transform(x)).long()
+
+    def embed(self, dim: int, **kwargs):
+        self.embeddings = torch.nn.Embedding(self.count, dim, **kwargs)
+
+    def look_up(self, x: torch.Tensor) -> torch.Tensor: # B x d
+        self.embeddings: torch.nn.Embedding
+        return self.embeddings(x)
+
 
 
 class DenseField(Field):
@@ -61,4 +96,39 @@ class DenseField(Field):
         return (x * self.scale_ + self.min_).float()
 
 
+class LabelField(SparseField): ...
+class TagetField(DenseField):
+
+    def fit(self, lower: float, upper: float, bound: Tuple = (0, 1)):
+        ...
+
+    def transform(self, x: torch.Tensor) -> torch.Tensor:
+        return x
+
+
+class Tokenizer(torch.nn.Module):
+
+    def __init__(self, fields: Iterable[Field]) -> None:
+        super().__init__()
+
+        self.sparse = torch.nn.ModuleList(SparseField.filter(fields))
+        self.dense = torch.nn.ModuleList(DenseField.filter(fields))
+
+    def look_up(self, inputs: Dict[torch.Tensor]) -> List[torch.Tensor]:
+        """
+        Dict[torch.Tensor: (B, )] -> List[torch.Tensor: B x d]
+        """
+        return [field.look_up(inputs[field.name]) for field in self.sparse]
+
+    def collect_dense(self, inputs: Dict[torch.Tensor]) -> torch.Tensor:
+        """
+        Dict[torch.Tensor: (B, )] -> torch.Tensor: B x len(self.dense)
+        """
+        return torch.stack([inputs[field.name] for field in self.dense], dim=1)
+
+    def flatten_cat(self, inputs: List[torch.Tensor]) -> torch.Tensor:
+        """
+        List[torch.Tensor: B x k x d] -> torch.Tensor: B x (k1 x d1 + k2 x d2 + ...)
+        """
+        return torch.cat([input_.flatten(1) for input_ in inputs])
 
