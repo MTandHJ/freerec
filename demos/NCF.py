c17524867e241cf824c69574b763edcea8c605c9
MTandHJ <congxueric@gmail.com>
2022-08-15 18:36:39 +0800

    modify NCF

--- demos/NCF.py
+++ demos/NCF.py
@@ -4,7 +4,6 @@ from typing import Dict, List
 
 import torch
 
-import freerec
 from freerec.dict2obj import Config
 from freerec.parser import Parser
 from freerec.launcher import Coach
@@ -31,6 +30,7 @@ class MovieLens1M_(MovieLens1M):
             SparseField(name='ItemID', na_value=0, dtype=int, tags=[ITEM, ID, FEATURE]),
         ],
         dense = [DenseField(name="Timestamp", na_value=0., dtype=float, transformer='none', tags=FEATURE)],
+        # target: 0|1
         target = [DenseField(name='Rating', na_value=None, dtype=int, transformer=Binarizer(threshold=1), tags=TARGET)]
     )
 
@@ -52,11 +52,9 @@ class CoachForNCF(Coach):
             users = {name: val.to(self.device) for name, val in users.items()}
             items = {name: val.to(self.device) for name, val in items.items()}
             targets = targets.to(self.device)
-            m = targets.size(0)
 
             preds = self.model(users, items)
-            n = len(preds) // m
-            preds = preds.view(m, n)
+            m, n = preds.size()
             targets = targets.repeat((1, n))
             targets[:, 1:].fill_(0)
             loss = self.criterion(preds, targets)
@@ -65,10 +63,9 @@ class CoachForNCF(Coach):
             loss.backward()
             self.optimizer.step()
             
-            self.callback(loss.item(), n=targets.size(0), mode="mean", prefix='train', pool=['LOSS'])
+            self.callback(loss.item(), n=m * n, mode="mean", prefix='train', pool=['LOSS'])
 
         self.lr_scheduler.step()
-
 
 
     def evaluate(self, prefix: str = 'valid'):
@@ -82,11 +79,9 @@ class CoachForNCF(Coach):
             users = {name: val.to(self.device) for name, val in users.items()}
             items = {name: val.to(self.device) for name, val in items.items()}
             targets = targets.to(self.device)
-            m = targets.size(0)
 
             preds = self.model(users, items)
-            n = len(preds) // m
-            preds = preds.view(m, n)
+            m, n = preds.size()
             targets = targets.repeat((1, n))
             targets[:, 1:].fill_(0)
             loss = self.criterion(preds, targets)
@@ -94,7 +89,7 @@ class CoachForNCF(Coach):
             running_preds.append(preds.detach().clone().cpu())
             running_targets.append(targets.detach().clone().cpu())
 
-            self.callback(loss, n=targets.size(0), mode="mean", prefix=prefix, pool=['LOSS'])
+            self.callback(loss, n=m * n, mode="mean", prefix=prefix, pool=['LOSS'])
 
         running_preds = torch.cat(running_preds)
         running_targets = torch.cat(running_targets)
@@ -112,29 +107,27 @@ def main():
 
     cfg = Parser()
     cfg.add_argument("-eb", "--embedding_dim", type=int, default=8)
+    cfg.add_argument("-neg", "--num_negs", type=int, default=4)
     cfg.set_defaults(
         fmt="{description}={embedding_dim}={optimizer}-{lr}-{weight_decay}={seed}",
         description="NeuCF",
         root="../movielens",
-        epochs=200,
-        batch_size=1024,
-        buffer_size=10240,
+        epochs=20,
+        batch_size=256,
+        buffer_size=1024000,
         optimizer='adam',
         lr=1e-3,
-        weight_decay=1e-2,
+        weight_decay=0.,
     )
     cfg.compile()
 
     basepipe = MovieLens1M_(cfg.root)
-    datapipe = basepipe.dataframe_(buffer_size=cfg.buffer_size).encode_().sample_negative_(num_negatives=4)
+    datapipe = basepipe.dataframe_(buffer_size=cfg.buffer_size).encode_().sample_negative_(num_negatives=cfg.num_negs)
     datapipe = datapipe.chunk_(batch_size=cfg.batch_size).dict_().tensor_().group_()
 
     tokenizer_mf = Tokenizer(datapipe.fields)
     tokenizer_mf.embed(
         dim=cfg.embedding_dim, tags=(FEATURE, SPARSE)
-    )
-    tokenizer_mf.embed(
-        dim=cfg.embedding_dim, tags=(FEATURE, DENSE), linear=True
     )
     tokenizer_mlp = copy.deepcopy(tokenizer_mf)
     model = NeuCF(tokenizer_mf, tokenizer_mlp).to(cfg.DEVICE)
@@ -142,20 +135,22 @@ def main():
     if cfg.optimizer == 'sgd':
         optimizer = torch.optim.SGD(
             model.parameters(), lr=cfg.lr, 
-            momentum=cfg.momentum, weight_decay=cfg.weight_decay,
+            momentum=cfg.momentum,
             nesterov=cfg.nesterov
         )
     elif cfg.optimizer == 'adam':
         optimizer = torch.optim.Adam(
             model.parameters(), lr=cfg.lr,
-            betas=(cfg.beta1, cfg.beta2), weight_decay=cfg.weight_decay
+            betas=(cfg.beta1, cfg.beta2)
         )
-    lr_scheduler=torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    lr_scheduler=torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.1)
+    criterion = BCELoss()
+    criterion.regulate(model.parameters(), rtype='l2', weight=cfg.weight_decay)
 
     coach = CoachForNCF(
         model=model,
         datapipe=datapipe,
-        criterion=BCELoss(),
+        criterion=criterion,
         optimizer=optimizer,
         lr_scheduler=lr_scheduler,
         device=cfg.DEVICE
