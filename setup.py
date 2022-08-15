d289fd2c082cc4f16c560eb08b33c24a220d8386
MTandHJ <congxueric@gmail.com>
2022-08-15 18:41:01 +0800

    version: 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
 
 setuptools.setup(
   name="freerec",
-  version="0.0.2",
+  version="0.0.3",
   author="MTandHJ",
   author_email="congxueric@gmail.com",
   description="PyTorch library for recommender systems",
