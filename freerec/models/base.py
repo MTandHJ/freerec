7fbddef1fd4ef6a06422ec067d384007c03f962a
MTandHJ <congxueric@gmail.com>
2022-08-18 09:18:58 +0800

    add init

--- freerec/models/base.py
+++ freerec/models/base.py
@@ -9,4 +9,10 @@ __all__ = ['RecSysArch']
 
 class RecSysArch(nn.Module):
 
-    def initialize(self): ... # TODO:
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0.)
+            elif isinstance(m, nn.Embedding):
+                nn.init.xavier_normal_(m.weight)
