9b57b749d76ede87ba67080b34a229dca6bd2cb3
MTandHJ <congxueric@gmail.com>
2022-09-21 12:52:15 +0800

    add check_version

--- freerec/__init__.py
+++ freerec/__init__.py
@@ -9,3 +9,8 @@ import freerec.metrics as metrics
 import freerec.parser as parser
 import freerec.utils as utils
 from freerec.dict2obj import Config
+
+
+def check_version(version: str):
+    if version != __version__:
+        print(f"\033[1;31m [Warning] FreeRec version of {version} is required but current version is {__version__} \033[0m")
