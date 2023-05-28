f88b4f5a9b35daf4242aeb740525d13839176153
MTandHJ <congxueric@gmail.com>
2023-05-28 14:05:37 +0800

    session datasets

--- freerec/data/datasets/session/diginetica.py
+++ freerec/data/datasets/session/diginetica.py
@@ -6,6 +6,7 @@ from .base import SessionItemTimeTriplet
 __all__ = [
     'Diginetica',
     'Diginetica_250811_Chron',
+    'Diginetica_250712_Chron',
 ]
 
 
@@ -33,4 +34,22 @@ class Diginetica_250811_Chron(SessionItemTimeTriplet):
     +-----------+--------+--------------------+---------------+--------+--------+-------+------------------------+
     |   204061  | 42171  | 4.8475896913177925 |     989204    | 795142 | 96445  | 97617 | 0.00011495078825064125 |
     +-----------+--------+--------------------+---------------+--------+--------+-------+------------------------+
+    """
+
+
+class Diginetica_250712_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered Diginetica dataset.
+
+    Config:
+    -------
+    filename: diginetica
+    dataset: Diginetica
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (7, 1, 2)
+
+    Statistics:
+    -----------
     """
---
+++ freerec/data/datasets/session/yoochoose.py
@@ -0,0 +1,165 @@
+
+
+from .base import SessionItemTimeTriplet
+
+
+__all__ = [
+    'YooChooseBuys', 'YooChooseClicks',
+    'YooChooseBuys14_250811_Chron', 'YooChooseClicks14_250811_Chron',
+    'YooChooseBuys164_250811_Chron', 'YooChooseClicks164_250811_Chron',
+    'YooChooseBuys14_250712_Chron', 'YooChooseClicks14_250712_Chron',
+    'YooChooseBuys164_250712_Chron', 'YooChooseClicks164_250712_Chron',
+]
+
+
+class YooChooseBuys(SessionItemTimeTriplet):
+    ...
+
+
+class YooChooseClicks(SessionItemTimeTriplet):
+    ...
+
+
+class YooChooseBuys14_250811_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseBuys1/4 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-buys
+    dataset: YooChooseBuys14
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (8, 1, 1)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseBuys164_250811_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseBuys1/64 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-buys
+    dataset: YooChooseBuys164
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (8, 1, 1)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseClicks14_250811_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseClickss1/4 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-clicks
+    dataset: YooChooseClicks14
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (8, 1, 1)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseClicks164_250811_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseClickss1/64 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-clicks
+    dataset: YooChooseClicks164
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (8, 1, 1)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseBuys14_250712_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseBuys1/4 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-buys
+    dataset: YooChooseBuys14
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (7, 1, 2)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseBuys164_250712_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseBuys1/64 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-buys
+    dataset: YooChooseBuys164
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (7, 1, 2)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseClicks14_250712_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseClickss1/4 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-clicks
+    dataset: YooChooseClicks14
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (7, 1, 2)
+
+    Statistics:
+    -----------
+    """
+
+
+class YooChooseClicks164_250712_Chron(SessionItemTimeTriplet):
+    r"""
+    Chronologically-ordered YooChooseClickss1/64 dataset.
+
+    Config:
+    -------
+    filename: yoochoose-clicks
+    dataset: YooChooseClicks164
+    kcore4user: 2
+    kcore4item: 5
+    star4pos: 0
+    ratios: (7, 1, 2)
+
+    Statistics:
+    -----------
+    """
+
