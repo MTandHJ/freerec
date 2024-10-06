6a292dbbfea22d6a2f0b4ffad367ce56f28aa177
MTandHJ <congxueric@gmail.com>
2024-10-06 16:13:45 +0800

    ✨feature: DCN

--- examples/DCN/main.py
+++ examples/DCN/main.py
@@ -5,18 +5,21 @@ from typing import Dict, Tuple
 import torch
 import torch.nn as nn
 import freerec
+from freerec.data.tags import LABEL
 
 freerec.declare(version='1.0.1')
 
 cfg = freerec.parser.Parser()
-cfg.add_argument("--embedding-dim", type=int, default=64)
+cfg.add_argument("--embedding-dim", type=int, default=4)
 cfg.add_argument("--num-layers", type=int, default=3)
+cfg.add_argument("--hidden-dims", type=str, default='64,32')
+cfg.add_argument("--hidden-dropout-rate", type=float, default=0.)
 
 cfg.set_defaults(
     description="DCN",
     root="../../data",
-    dataset='Criteo',
-    epochs=1000,
+    dataset='Frappe_x1_BARS',
+    epochs=100,
     batch_size=2048,
     optimizer='adam',
     lr=1e-3,
@@ -40,111 +43,103 @@ class CrossInteraction(nn.Module):
         return interact_out
 
 
-class DCN(freerec.models.GenRecArch):
+class MLPBlock(nn.Module):
 
     def __init__(
-        self, dataset: freerec.data.datasets.RecDataSet,
-        embedding_dim: int = 64, num_layers: int = 3
+        self,
+        input_dim: int,
+        output_dim: int,
+        dropout_rate: float = 0.
+    ) -> None:
+        super().__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+        self.act = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+    
+    def forward(self, x: torch.Tensor):
+        x = self.linear(x)
+        x = self.act(x)
+        x = self.dropout(x)
+        return x
+
+
+class DCN(freerec.models.PredRecArch):
+
+    def __init__(
+        self, dataset: freerec.data.datasets.RecDataSet
     ) -> None:
         super().__init__(dataset)
 
-        self.num_layers = num_layers
+        self.num_layers = cfg.num_layers
 
-        for field in self.fields:
+        self.input_fields = self.fields.match_not(LABEL)
+
+        input_dim: int = 0
+        for field in self.input_fields:
             field.add_module(
                 "embeddings", nn.Embedding(
-                    field.count, embedding_dim
+                    field.count, cfg.embedding_dim
                 )
             )
 
-        input_dim = len(self.fields) * embedding_dim
+        input_dim += len(self.input_fields) * cfg.embedding_dim
 
-        self.dnn = nn.Sequential(
-            nn.Linear(input_dim, 64),
-            nn.ReLU(),
-            nn.Dropout(0.),
-            nn.Linear(64, 32),
-            nn.ReLU()
-        )
+        hidden_dims = [input_dim] + list(map(int, cfg.hidden_dims.split(',')))
+        blocks = []
+        for input_dim, output_dim in zip(hidden_dims[:-1], hidden_dims[1:]):
+            blocks.append(
+                MLPBlock(
+                    input_dim,
+                    output_dim,
+                    dropout_rate=cfg.hidden_dropout_rate
+                )
+            )
+        self.dnn = nn.Sequential(*blocks) # input_dim -> hidden_dims[-1]
 
-        self.crossnet = nn.ModuleList(
+        self.crossnet = nn.ModuleList( # input_dim -> input_dim
             [CrossInteraction(input_dim) for _ in range(3)]
         )
 
-        self.fc = nn.Linear(input_dim + 32, 1)
+        self.fc = nn.Linear(input_dim + hidden_dims[-1], 1)
 
         self.criterion = freerec.criterions.BCELoss4Logits(reduction='mean')
 
         self.reset_parameters()
 
-    def reset_parameters(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.)
-            elif isinstance(m, nn.Embedding):
-                nn.init.normal_(m.weight, std=1.e-4)
-            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
-                nn.init.constant_(m.weight, 1.)
-                nn.init.constant_(m.bias, 0.)
-
     def sure_trainpipe(self, batch_size: int):
-        return self.dataset.train().choiced_user_ids_source(
-        ).gen_train_sampling_pos_().gen_train_sampling_neg_(
-            num_negatives=1
-        ).batch_(batch_size).tensor_()
+        return self.dataset.train().shuffled_inter_source().batch_(
+            batch_size
+        ).tensor_()
 
     def encode(self, data: Dict[freerec.data.fields.Field, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
-        embeddings = torch.cat([field.embeddings(data[field]) for field in self.fields], dim=-1) # (B, D)
+        embeddings = torch.cat(
+            [field.embeddings(data[field]).flatten(1) for field in self.input_fields], 
+            dim=-1
+        ) # (B, D)
         dnnEmbds = self.dnn(embeddings) # (B, D')
-        crossEmbds = self.crossnet(embeddings) # (B, D)
+        crossEmbds = embeddings
+        for crosslayer in self.crossnet:
+            crossEmbds = crosslayer(embeddings, crossEmbds)
         finalEmbds = torch.cat((dnnEmbds, crossEmbds), dim=-1) # (B, D + D')
         return self.fc(finalEmbds) # (B, 1)
 
     def fit(self, data: Dict[freerec.data.fields.Field, torch.Tensor]):
         logits = self.encode(data)
-        labels = self.data[self.Label] # (B, 1)
+        labels = data[self.Label] # (B, 1)
         rec_loss = self.criterion(logits, labels)
         return rec_loss
 
     def recommend_from_pool(self, data: Dict[freerec.data.fields.Field, torch.Tensor]):
-        logits = self.encode(data).flatten() # (B,)
+        logits = self.encode(data)
         return logits.sigmoid() # scores
 
 
-class CoachForLightGCN(freerec.launcher.Coach):
-
-    def set_optimizer(self):
-        if self.cfg.optimizer.lower() == 'sgd':
-            self.optimizer = torch.optim.SGD(
-                self.model.parameters(), lr=self.cfg.lr, 
-                momentum=self.cfg.momentum,
-                nesterov=self.cfg.nesterov,
-                # weight_decay=self.cfg.weight_decay
-            )
-        elif self.cfg.optimizer.lower() == 'adam':
-            self.optimizer = torch.optim.Adam(
-                self.model.parameters(), lr=self.cfg.lr,
-                betas=(self.cfg.beta1, self.cfg.beta2),
-                # weight_decay=self.cfg.weight_decay
-            )
-        elif self.cfg.optimizer.lower() == 'adamw':
-            self.optimizer = torch.optim.AdamW(
-                self.model.parameters(), lr=self.cfg.lr,
-                betas=(self.cfg.beta1, self.cfg.beta2),
-                # weight_decay=self.cfg.weight_decay
-            )
-        else:
-            raise NotImplementedError(
-                f"Unexpected optimizer {self.cfg.optimizer} ..."
-            )
+class CoachForDCN(freerec.launcher.Coach):
 
     def train_per_epoch(self, epoch: int):
         for data in self.dataloader:
             data = self.dict_to_device(data)
-            rec_loss, emb_loss = self.model(data)
-            loss = rec_loss + self.cfg.weight_decay * emb_loss
+            loss = self.model(data)
 
             self.optimizer.zero_grad()
             loss.backward()
@@ -162,18 +157,15 @@ def main():
     try:
         dataset = getattr(freerec.data.datasets, cfg.dataset)(root=cfg.root)
     except AttributeError:
-        dataset = freerec.data.datasets.RecDataSet(cfg.root, cfg.dataset, tasktag=cfg.tasktag)
+        dataset = freerec.data.datasets.PredictionRecDataSet(cfg.root, cfg.dataset, tasktag=cfg.tasktag)
 
-    model = LightGCN(
-        dataset,
-        embedding_dim=cfg.embedding_dim, num_layers=cfg.num_layers
-    )
+    model = DCN(dataset)
 
     trainpipe = model.sure_trainpipe(cfg.batch_size)
-    validpipe = model.sure_validpipe(cfg.ranking)
-    testpipe = model.sure_testpipe(cfg.ranking)
+    validpipe = model.sure_validpipe()
+    testpipe = model.sure_testpipe()
 
-    coach = CoachForLightGCN(
+    coach = CoachForDCN(
         dataset=dataset,
         trainpipe=trainpipe,
         validpipe=validpipe,
